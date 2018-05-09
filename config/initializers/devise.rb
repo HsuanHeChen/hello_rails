eddcd9557dfc4f186ebf628f82753ea2dc1ed110
HsuanHeChen <mars80408@gmail.com>
2018-05-10 00:22:52 +0800

    no message

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -248,7 +248,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  config.omniauth :facebook, Settings.FACEBOOK_APP_ID, Settings.FACEBOOK_SECRET, scope: 'email', info_fields: 'email,name'
+  config.omniauth :facebook, Settings.FACEBOOK_APP_ID, Settings.FACEBOOK_SECRET, scope: 'email', 
+    info_fields: 'email,name', callback_url: 'https://hellorails.intelligentcarry.com/'
 
   # ==> Warden configuration
   # If you want to use other strategies, that are not supported by Devise, or
